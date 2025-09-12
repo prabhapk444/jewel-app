da8a76a7e02816ddb754c795395550b943b07f9c
prabhapk444 <karanprabha22668@gmail.com>
2025-09-12 07:43:53 +0530

    first commit

---
+++ app/src/main/java/com/example/sriramjewellers/SplashScreen.kt
@@ -0,0 +1,60 @@
+package com.example.sriramjewellers
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.Image
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+import androidx.compose.ui.graphics.Color
+class SplashScreen : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            SplashContent {
+
+                startActivity(Intent(this, MainActivity::class.java))
+                finish() // Close SplashScreen
+            }
+        }
+    }
+}
+
+@Composable
+fun SplashContent(onTimeout: () -> Unit) {
+
+    LaunchedEffect(key1 = true) {
+        delay(2000)
+        onTimeout()
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+                modifier = Modifier.size(150.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Sriram Jewellers",
+                fontSize = 24.sp,
+                color = Color(0xFFFFB800)
+
+            )
+        }
+    }
+}
